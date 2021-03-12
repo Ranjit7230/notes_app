4d347b745cf606bd9cc230efae81e6eddca06565
Ranjit Nayak <ranjitn559@gmail.com>
2021-03-12 12:22:48 +0530

    created a new activity for splash screen

---
+++ app/src/main/java/com/example/notes/Splash_screen.java
@@ -0,0 +1,26 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_screen extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent=new Intent(Splash_screen.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
